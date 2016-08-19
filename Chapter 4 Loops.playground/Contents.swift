0b73560fa6846cdf2a3131327e4206aef4318cd3
Jackson Taylor <closprieur@gmail.com>
2016-08-19 17:14:01 +1000

    Add questions

--- Chapter 4 Loops.playground/Contents.swift
+++ Chapter 4 Loops.playground/Contents.swift
@@ -1,5 +1,239 @@
-//: Playground - noun: a place where people can play
+/*
+ 
+ WE HEART SWIFT EXERCISES
+ 
+ Source: https://www.weheartswift.com/
+ 
+ Swift 2.2
+ 
+ */
+
+// CHAPTER 4 LOOPS
 
 import UIKit
 
 var str = "Hello, playground"
+
+/*
+ 
+ 4.1 Chalkboard
+ 
+ Write a program that writes “I will not skip the fundamentals!” N times.
+ 
+ var N = 10
+ 
+*/
+
+
+
+/*
+ 
+ 4.2 Squares
+ 
+ Print the first N square numbers. A square number, also called perfect square, is an integer that is obtained by squaring some other integer; in other words, it is the product of some integer with itself (ex. 1, 4 = 2 * 2, 9 = 3 *3 …).
+ 
+ var N = 10
+ 
+*/
+
+
+
+/*
+ 
+ 4.3 Powers of 2
+ 
+ Print the powers of 2 that are less than or equal to N.
+ 
+ var N = 10
+ 
+*/
+
+
+
+/*
+ 
+ 4.4 Alternative Counting
+ 
+ Write all the numbers from 1 to N in alternative order, one number from the left side (starting with one) and one number from the right side (starting from N down to 1).
+ 
+ var N = 5
+ 
+*/
+
+
+
+/*
+ 
+ 4.5 Square
+ 
+ Given an integer N draw a square of N x N asterisks. Look at the examples.
+ 
+ var N = 4
+ 
+*/
+
+
+
+/*
+ 
+ 4.6 Rectangle
+ 
+ Given two integers N and M draw a rectangle of N x M asterisks. Look at the examples.
+ 
+ var N = 3
+ var M = 7
+ 
+ */
+
+
+
+/*
+ 
+ 4.7 Triangle
+ 
+ Given an integer N draw a triangle of asterisks. The triangle should have N lines, the i-th line should have iasterisks on it.
+ 
+ var N = 4
+ 
+ */
+
+
+
+/*
+ 
+ 4.8 Pyramid
+ 
+ Given an integer N draw a pyramid of asterisks. The pyramid should have N lines. On the i-th line there should be N-i spaces followed by i*2-1 asterisks.
+ 
+ var N = 3
+ 
+ */
+
+
+
+/*
+ 
+ 4.9 Rhombus
+ 
+ Given an integer N draw a rhombus of asterisks, like the ones in the examples.
+ 
+ var N = 4
+ 
+ */
+
+
+
+/*
+ 
+ 4.10 Aztec Pyramid
+ 
+ Given an integer N draw a Aztec pyramid of asterisks, like the ones in the examples.
+ 
+ var N = 3
+ 
+ */
+
+
+
+/*
+ 
+ 4.11 Chess Board
+ 
+ Given an integer N draw a chess board of size N x N. Each line of the chess board should have spaces and number signs(#) alternating. A space represents a white cell and the number sign a black one. The chess board should be bordered using +, - and | like in the examples below.
+ 
+ var N = 8
+ 
+ */
+
+/*
+ 
+ 4.12 Fibonacci
+ 
+ Write a program that prints the first N Fibonacci numbers. The first two Fibonacci numbers are 1, the rest of the elements are the sum of the previous two. The first seven numbers are 1, 1, 2, 3, 5, 8 and 13.
+ 
+ var N = 3
+ 
+*/
+
+
+
+/*
+ 
+ 4.13 Leap Years
+ 
+ Write a program that prints the next N leap years starting with leapYear. A leap year is a year containing an extra day. It has 366 days instead of the normal 365 days. The extra day is added in February, which has 29 daysinstead of the normal 28 days. Leap years occur every 4 years, 2012 was a leap year and 2016 will be a leap year.
+ Except that every 100 years special rules apply. Years that are divisible by 100 are not leap years if they are not divisible by 400. For example 1900 was not a leap year, but 2000 was.
+ 
+ var N = 3
+ 
+ // the current leap year
+ var leapYear = 2016
+ 
+*/
+
+
+
+/*
+
+ 4.14 Reverse
+ 
+ You are given a number. Print the number with the digits in reversed order.
+ 
+ var number = 1234
+ 
+ */
+
+
+
+/*
+ 
+ 4.15 GCD
+ 
+ You are given two numbers a and b. Find and print the greatest common divisor of a and b.
+ The greatest common divisor of a and b is the largest number that divides both a and b.
+ 
+ var a = 24
+ var b = 18year
+ var leapYear = 2016
+ 
+ */
+
+
+
+/*
+ 
+ 4.16 Prime numbers
+ 
+ You are given a number. Print "prime" if the number is a prime and "not prime" otherwise.
+ A number is a prime if it has exactly 2 distinct divisors (1 and itself).
+ 
+ var number = 17
+ 
+ */
+
+
+
+/*
+ 
+ 4.17 Factoring numbers
+ 
+ You are given a number. Decompose number into prime factor and write it as an expression(see examples).
+ 
+ var number = 10
+ 
+ */
+
+
+
+/*
+ 
+ 4.18 Free of squares
+ 
+ Find all numbers free of squares less than or equal to N. A number is free of square if it cannot be divided by any square number except 1.
+ 
+ var N = 10
+ 
+*/
+
+
+

b492709ad9a825d3ffb77d0d08c888aec67d82a7
Jackson Taylor <closprieur@gmail.com>
2016-08-20 19:17:39 +1000

    Add questions

---
+++ Chapter 6 Arrays.playground/Contents.swift
@@ -0,0 +1,205 @@
+/*
+ 
+ WE HEART SWIFT EXERCISES
+ 
+ Source: https://www.weheartswift.com/
+ 
+ Swift 2.2
+ 
+ */
+
+// CHAPTER 6 ARRAYS
+
+
+
+/*
+ 
+ 6.1 Max
+ 
+ Print the maximum value from listOfNumbers.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+ 
+*/
+
+
+
+/*
+ 
+ 6.2 Odd
+ 
+ Print all the odd numbers from listOfNumbers.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+ 
+*/
+
+
+
+/*
+ 
+ 6.3 Sum
+ 
+ Print the sum of all the numbers from listOfNumbers.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+ 
+ */
+
+
+
+/*
+ 
+ 6.4 Odd Index
+ 
+ Print all the numbers from listOfNumbers that are located at odd indexes.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+ 
+ */
+
+
+
+/*
+ 
+ 6.5 Going back
+ 
+ Print the numbers from listOfNumbers in reverse order on separate lines.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+ 
+*/
+
+
+
+/*
+ 
+ 6.6 Reverse
+ 
+ Reverse the order of the elements in listOfNumbers without creating any additional arrays.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+
+ 
+*/
+
+
+
+/*
+ 
+ 6.7 Sorting
+ 
+ Sort the values in listOfNumbers in descending order.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+ 
+*/
+
+
+
+/*
+
+ 6.8 Search
+ 
+ Find out if x appears in listOfNumbers. Print yes if true and no otherwise.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+ var x = 10
+ 
+*/
+
+
+
+/*
+ 
+ 6.9 Intersection
+ 
+ Print all the elements from otherNumbers that appear in listOfNumbers. Don’t print anything if listOfNumbers andotherNumbers have no common elements.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+ var otherNumbers = [1, 2, 3, 4, 5, 6]
+
+ 
+*/
+
+
+
+/*
+ 
+ 6.10 Divisors
+ 
+ Print all the numbers from listOfNumbers that are divisible by at least one number from divisors.
+ 
+ var listOfNumbers = [1, 2, 3, 10, 100]
+ var divisors = [2, 5]
+ 
+*/
+
+
+
+/*
+ 
+ 6.11 Greatest divisor of all
+ 
+ Find and print the greatest common divisor of all the numbers in numbers. A common divisor of a list of numbers is a number that divides all of them.
+ 
+ var numbers = [12, 36, 720, 18]
+ 
+*/
+
+
+
+/*
+ 
+ 6.12 Fibonacci
+ 
+ Generate the first N numbers in the fibonacci sequence and store them in an array named fibonacci.
+ 
+ var N = 10
+ 
+*/
+
+
+
+/*
+ 
+ 6.13 Divisors
+ 
+ Given a number find and store all it’s divisors in an array called divisors, then print the divisors in ascending order on separate lines.
+ 
+ var number = 60
+ var divisors: [Int] = []
+ 
+*/
+
+
+
+/*
+ 
+ 6.14 Digits
+ 
+ Find and store the digits of number from left to right in an array digits, and then print the digits on separate lines.
+ 
+ var number = 12345
+ var digits: [Int] = []
+ 
+*/
+
+
+
+/*
+ 
+ 6.15 Unique
+ 
+ Create a list unique with all the unique numbers from listOfNumbers, and then print the numbers on separate lines.
+ 
+ var listOfNumbers = [1, 2, 3, 1, 2, 10, 100]
+ 
+ var unique: [Int] = []
+ 
+*/
+
+
+
+
+
